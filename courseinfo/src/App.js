60140990c17c922106477d278e4b9d06d0a8b108
Kevin Tong <122766486+kevint421@users.noreply.github.com>
2023-05-26 20:48:44 -0400

    courseinfo

---
+++ courseinfo/src/App.js
@@ -0,0 +1,60 @@
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  const parts = props.course.parts;
+
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const parts = props.course.parts;
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default App;
